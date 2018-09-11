c59025b64976b23060088bcff2415b04415dfa3a
Kartikei-12 <kartikeimittal@gmail.com>
2018-09-11 14:22:36 +0530

    Testing input methods

---
+++ Lab-6_11-09-2018/1.cpp
@@ -0,0 +1,17 @@
+//Author: Kartikei Mittal
+//ID: 2017KUCP1032
+//Date: 11-September-2018
+//Input methods
+
+#include<iostream>
+    using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char s1[15], s2[15];
+    cin.getline(s1, 10);
+    cout<<s1<<":First\n";
+    cin.get(s2, 10, ' ');
+    cout<<s1<<":Second\n";
+    return 0;
+}
